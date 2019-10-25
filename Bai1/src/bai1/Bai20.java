8f0b44626c1172a4179bcafe9ffdfa31395c4bb0
hailong <hailongnguyen1509@gmail.com>
2019-10-25 22:56:20 +0700

    final

--- Bai1/src/bai1/Bai20.java
+++ Bai1/src/bai1/Bai20.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Practice2;
+package bai1;
 
 import java.util.Scanner;
 import java.util.Stack;
