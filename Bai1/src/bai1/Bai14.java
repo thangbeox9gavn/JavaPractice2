217fe678a3667907c5c1e069ec7a650b823a4234
hailong <hailongnguyen1509@gmail.com>
2019-10-25 23:44:41 +0700

    adad

--- Bai1/src/bai1/Bai14.java
+++ Bai1/src/bai1/Bai14.java
@@ -5,10 +5,36 @@
  */
 package bai1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Admin
  */
 public class Bai14 {
-    
+    static long M = (long) (1e9 + 7);
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n, t;
+        t = sc.nextInt();
+        while (t-- > 0) {
+            n = sc.nextInt();
+            System.out.println(powM(2, n-1));
+        }
+    }
+
+    static long powM(int n, int k) {
+        if (k == 0) {
+            return 1;
+        }
+        long x = powM(n, k / 2);
+        if (k % 2 != 0) {
+            return (x * x % M) * n % M;
+        } else {
+            return (x * x)%M;
+        }
+
+    }
 }
